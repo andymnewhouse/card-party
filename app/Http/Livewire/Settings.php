e841040f6dc9c495c98011c6bc3d8f9d1c440bc2
Andy Swick <andymswick@gmail.com>
2020-05-03 22:36:17 -0500

    Work on settings

--- app/Http/Livewire/Settings.php
+++ app/Http/Livewire/Settings.php
@@ -8,19 +8,19 @@ class Settings extends Component
 {
     public $name;
     public $email;
+    public $user;
 
     public function mount()
     {
         $this->title = 'My Settings';
         $this->name = auth()->user()->name;
         $this->email = auth()->user()->email;
+        $this->user = auth()->user();
     }
 
     public function render()
     {
-        return view('livewire.settings', [
-            'user' => auth()->user()
-        ]);
+        return view('livewire.settings');
     }
 
     public function deleteAccount()
@@ -41,5 +41,26 @@ class Settings extends Component
 
     public function updateProfile()
     {
+        if ($this->name !== $this->user->name && $this->email !== $this->user->email) {
+            $this->user->name = $this->name;
+            $this->user->email = $this->email;
+            $this->user->email_verified_at = null;
+            $this->user->save();
+            $this->user->sendEmailVerificationNotification();
+
+            $this->emit('notify', ['message' => 'Both your name and email were updated. Please check your email to verify the new address.', 'type' => 'success']);
+        } elseif ($this->name !== $this->user->name) {
+            $this->user->name = $this->name;
+            $this->user->save();
+
+            $this->emit('notify', ['message' => 'Your name was successfully updated.', 'type' => 'success']);
+        } else {
+            $this->user->email = $this->email;
+            $this->user->email_verified_at = null;
+            $this->user->save();
+            $this->user->sendEmailVerificationNotification();
+
+            $this->emit('notify', ['message' => 'Your email was successfully updated. Please check your email to verify the new address.', 'type' => 'success']);
+        }
     }
 }
