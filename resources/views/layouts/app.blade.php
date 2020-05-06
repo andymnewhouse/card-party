ba0feaf3141b15b2757473544b27e95f589a5722
Andy Swick <andymswick@gmail.com>
2020-05-05 20:35:42 -0500

    Use asset for favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
 
     <title>{{ $title ?? ''}} - {{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/img/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#f16061">
-    <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('/img/favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('/img/favicon/safari-pinned-tab.svg" color="#f16061') }}">
+    <link rel="shortcut icon" href="{{ asset('/img/favicon/favicon.ico') }}">
     <meta name="msapplication-TileColor" content="#f16061">
-    <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+    <meta name="msapplication-config" content="{{ asset('/img/favicon/browserconfig.xml') }}">
     <meta name="theme-color" content="#ffffff">
 
     <!-- Scripts -->
